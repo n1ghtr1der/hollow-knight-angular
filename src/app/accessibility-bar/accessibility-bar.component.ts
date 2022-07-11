85d1cdd4b44212424933f879abfc5d57c169782a
Emirton Sousa <emirtonf@gmail.com>
2022-07-11 14:51:13 -0300

    feat: accessibility bar function added.

--- src/app/accessibility-bar/accessibility-bar.component.ts
+++ src/app/accessibility-bar/accessibility-bar.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accessibility-bar.component.css']
 })
 export class AccessibilityBarComponent implements OnInit {
+  element = document.querySelector("html")!;
+  fontSize = getComputedStyle(this.element).getPropertyValue("font-size");
+  
 
+  
   constructor() { }
 
   ngOnInit(): void {
+    localStorage.setItem("globalFontSize", this.fontSize);
+    console.log(this.fontSize);
+  }
+
+  changeSize() {
+    this.element.style.fontSize = '24px';
+    console.log('teste');
+  }
+
+  changeFontSize(type: string) {
+    let element = document.querySelector("html")!;
+    let size: string | number = getComputedStyle(element).getPropertyValue("font-size");
+    size = parseInt(size);
+    switch(true) {
+      case type == 'increase-font' && size < 22:
+        this.element.style.fontSize = (size + 2) + "px";
+        break;
+      case type == 'decrease-font' && size > 10:
+        this.element.style.fontSize = (size - 2) + "px";
+        break;
+      case type == 'default-font':
+        this.element.style.fontSize = localStorage.getItem('globalFontSize')!;
+    }
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component, Output} from '@angular/core';
+import { Component, Output, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+
+export class AppComponent implements OnInit {
+
+
+  ngOnInit(): void {
+  }
 }
